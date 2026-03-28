2a905aef55a9219cff1e1c550e6d0321d488df36
JoV1206 <102318919+JoV1206@users.noreply.github.com>
2026-03-28 17:59:54 -0300

    Update and rename fiat-app.jsx to author.jsx

---
+++ models/author.jsx
@@ -0,0 +1,4 @@
+# ==================================================================================================================================================== #        
+# Author: João Vitor Almeida Teodoro                                                                                                                   #
+# Project: Uma Arquitetura Híbrida de Autenticação Biométrica com Detecção de Vivacidade Multicamadas e Integração ao Sistema Operacional Windows      #
+# ==================================================================================================================================================== #
